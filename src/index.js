a0e5b110d487ce37d5f65ebbf64d260c38e4f793
franky3-git <frankndounga@yahoo.fr>
2020-12-27 22:09:27 +0100

    remaking structure in index.js

--- src/index.js
+++ src/index.js
@@ -4,33 +4,29 @@ const items = require('./js/data');
 
 
 //set initial cart
-let cart;
-if(localStorage.getItem('cart')) {
-	cart = JSON.parse(localStorage.getItem('cart'));
-} else {
-	cart = [];
-}
+let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
 
-//SELECT BUTTONS
-const btnsRemoveFromCart = document.getElementsByClassName('cart-item-delete');
-const btnClearCart = document.getElementsByClassName('btn-clear-cart')[0];
-const btnCartBox = document.getElementsByClassName('btn-cart-box')[0];
-const btnAddToCart = document.getElementsByClassName('btn-add-to-cart');
-
-//HTML ELEMENTS
-const totalCart = document.getElementsByClassName('total')[0];
-const cartItems = document.getElementsByClassName('cart-item');
-const nberItemsInCart = document.getElementsByClassName('nber-item')[0];
-const totalInBtnCartBox = document.getElementsByClassName('total-cart')[0];
-const cartBox = document.getElementsByClassName('cart-box')[0];
-const cartItemsBox = document.getElementsByClassName('cart-items')[0];
-const shopItemsBox = document.getElementsByClassName('shop-items-box')[0];
-const template1 = document.querySelector('.template1');
-const template2 = document.querySelector('.template2');
 
 document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
+	//SELECT BUTTONS
+	const btnsRemoveFromCart = document.getElementsByClassName('cart-item-delete');
+	const btnClearCart = document.getElementsByClassName('btn-clear-cart')[0];
+	const btnCartBox = document.getElementsByClassName('btn-cart-box')[0];
+	const btnAddToCart = document.getElementsByClassName('btn-add-to-cart');
+	
+	//HTML ELEMENTS
+	const totalCart = document.getElementsByClassName('total')[0];
+	const cartItems = document.getElementsByClassName('cart-item');
+	const nberItemsInCart = document.getElementsByClassName('nber-item')[0];
+	const totalInBtnCartBox = document.getElementsByClassName('total-cart')[0];
+	const cartBox = document.getElementsByClassName('cart-box')[0];
+	const cartItemsBox = document.getElementsByClassName('cart-items')[0];
+	const shopItemsBox = document.getElementsByClassName('shop-items-box')[0]; //
+	const template1 = document.querySelector('.template1'); //
+	const template2 = document.querySelector('.template2'); //
+
 	init();
 	updateTotal();
 	
@@ -50,89 +46,97 @@ function ready() {
 		}
 	});
 
+	//add event on each button in the item
 	Array.prototype.slice.call(btnAddToCart).forEach(btnAdd => {
 		btnAdd.addEventListener('click', putInCart)
 	});
-}
-
-// FUNCTION
-function init() {
-	shopItemsBox.innerHTML = '';
-	items.forEach(currentItem => {
-		var html = template2.textContent;
-		html = html.replace('%name%', currentItem.name);
-		html = html.replace('%price%', currentItem.price);
-		html = html.replace('%img%', currentItem.img);
-		
-		shopItemsBox.innerHTML += html;
-	});
-	renderCartItems();
-}
 
 
-function removeItemInCart (event) {
-	const target = event.target.parentElement;
-	const nameItemRemove = target.querySelector('.cart-item-name').textContent;
-	const indexToRemove = cart.findIndex(item => nameItemRemove == item.name);
-	cart.splice(indexToRemove, 1);
-	renderCartItems();
-	saveCart();
-}
+	//FUNCTIONS
+	function init() {
+		shopItemsBox.innerHTML = '';
+		items.forEach(currentItem => {
+			var html = template2.textContent;
+			html = html.replace('%name%', currentItem.name);
+			html = html.replace('%price%', currentItem.price);
+			html = html.replace('%img%', currentItem.img);
+			
+			shopItemsBox.innerHTML += html;
+		});
+		renderSelectedItems();
+	}
 
-function clearCart() {
-	if(confirm('Are you sure you want to clear the cart?')) {
-		cart = [];
-		renderCartItems();
-		saveCart();
-	}	
-}
-
-function putInCart (e) {
-	const item = e.target.parentElement.parentElement;
-	const name = item.querySelector('.shop-item-name').firstElementChild.textContent;
-	const price = item.getElementsByClassName('actual-price')[0].textContent;
-	const srcImage = item.getElementsByClassName('shop-item-img')[0].firstElementChild.src;
+	function renderSelectedItems () {
+		cartItemsBox.innerHTML = '';
+		cart.forEach(item => {
+			
+			let html = template1.textContent;
+			html = html.replace('%name%', item.name);
+			html = html.replace('%price%', '$'+ item.price);
+			html = html.replace('%img%', item.src);
 	
-	const newItem = {name, price, src: srcImage};
-	cart.push(newItem);
-	renderCartItems();
-	saveCart();
-}
+			cartItemsBox.innerHTML += html;
+		});
+		updateTotal();
+	}
 
-function renderCartItems () {
-	cartItemsBox.innerHTML = '';
-	cart.forEach(item => {
+	function updateTotal () {
+		var total = 0;
+		var nb = 0;
+		/*if(!cart) {
+			return;
+		}*/
+		cart.forEach(item => {
+			nb++;
+			const priceItem = item.price;
+			total += parseFloat(priceItem);
+			
+		});
+		total = (total).toFixed(2);
+
+		//return [];
+		displayTotal(total, nb)
+	}
+
+	function displayTotal(total, nb) {
+		totalCart.textContent = '$ '+ total;
+		nberItemsInCart.textContent = nb;
+		totalInBtnCartBox.textContent = total;
+	}
+
+	function putInCart (e) {
+		const item = e.target.parentElement.parentElement;
+		const name = item.querySelector('.shop-item-name').firstElementChild.textContent;
+		const price = item.getElementsByClassName('actual-price')[0].textContent;
+		const srcImage = item.getElementsByClassName('shop-item-img')[0].firstElementChild.src;
 		
-		let html = template1.textContent;
-		html = html.replace('%name%', item.name);
-		html = html.replace('%price%', '$'+ item.price);
-		html = html.replace('%img%', item.src);
+		const newItem = {name, price, src: srcImage};
+		cart.push(newItem);
+		renderSelectedItems();
+		saveCart();
+	}
 
-		cartItemsBox.innerHTML += html;
-	});
-	updateTotal();
+	function clearCart() {
+		if(confirm('Are you sure you want to clear the cart?')) {
+			cart = [];
+			renderSelectedItems();
+			saveCart();
+		}	
+	}
+
+	function removeItemInCart (event) {
+		const target = event.target.parentElement;
+		const nameItemRemove = target.querySelector('.cart-item-name').textContent;
+		const indexToRemove = cart.findIndex(item => nameItemRemove == item.name);
+		cart.splice(indexToRemove, 1);
+		renderSelectedItems();
+		saveCart();
+	}
+	
 }
 
 function saveCart() {
 	localStorage.setItem('cart', JSON.stringify(cart));
-}
-
-function updateTotal () {
-	var total = 0;
-	var nb = 0;
-	/*if(!cart) {
-		return;
-	}*/
-	cart.forEach(item => {
-		nb++;
-		const priceItem = item.price;
-		total += parseFloat(priceItem);
-		
-	});
-	total = (total).toFixed(2);
-	totalCart.textContent = '$' + total;
-	totalInBtnCartBox.textContent = total;
-	nberItemsInCart.textContent = nb;
 }
 
 //localStorage.clear();
