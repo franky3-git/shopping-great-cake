d30c70335e82516623e6e550f7c66734d7becbd9
franky3-git <frankndounga@yahoo.fr>
2020-12-27 22:38:37 +0100

    finishing work in the rebuildCode branch

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.js',
         publicPath: 'dist'
-    },
+    }
     // devServer: {
     //     contentBase: path.resolve(__dirname, 'dist'),
     //     port: 3000
